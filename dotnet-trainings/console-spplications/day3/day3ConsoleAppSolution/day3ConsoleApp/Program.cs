469b78598da4e4462c9ac7b1f295262606932fc7
mv <mrramidua@gmail.com>
2024-04-14 18:02:28 +0530

    Day 3 re-update

---
+++ dotnet-trainings/console-spplications/day3/day3ConsoleAppSolution/day3ConsoleApp/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        ArithmeticOperations();
+    }
+    //Console.WriteLine("Hello, World!");
+    //string name = null;
+    //int num1;
+    //int num2;
+    //num1 = Convert.ToInt32(name);
+    //num2 = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine(num1);
+    //Console.WriteLine(num2);
+    static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    static int Mod(int num1, int num2)
+    {
+        return num1 % num2;
+    }
+    static int Sub(int num1, int num2)
+    {
+        return num2 - num1;
+    }
+    static int Mul(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+    static int Div(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+
+    static int GetInput()
+    {
+        int n1;
+        Console.WriteLine("Please enter the the number");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        return n1;
+    }
+
+    static void Printoutput(int result)
+    {
+        Console.WriteLine("The result is" + result);
+    }
+
+    static void ArithmeticOperations()
+    {
+        int num1 = GetInput();
+        int num2 = GetInput();
+        int sum = Add(num1, num2);
+        int diff = Sub(num1, num2);
+        int prod = Mul(num1, num2);
+        int div = Div(num1, num2);
+        int remainder = Mod(num1, num2);
+
+        Printoutput(sum);
+        Printoutput(diff);
+        Printoutput(remainder);
+        Printoutput(prod);
+        Printoutput(div);
+    }
+
+
+}
