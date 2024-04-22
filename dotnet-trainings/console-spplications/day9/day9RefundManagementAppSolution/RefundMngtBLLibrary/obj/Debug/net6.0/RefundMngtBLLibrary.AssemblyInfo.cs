3756f164af28d34daf41271b0a9a0f18eb58b278
mv <mrramidua@gmail.com>
2024-04-22 17:58:15 +0530

    Final Commit

--- dotnet-trainings/console-spplications/day9/day9RefundManagementAppSolution/RefundMngtBLLibrary/obj/Debug/net6.0/RefundMngtBLLibrary.AssemblyInfo.cs
+++ dotnet-trainings/console-spplications/day9/day9RefundManagementAppSolution/RefundMngtBLLibrary/obj/Debug/net6.0/RefundMngtBLLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefundMngtBLLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb229fa6d70e8d6a04f14b98e80f31e25ea0dfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a30d0718591234504f3f4986bbf218787abdce")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefundMngtBLLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefundMngtBLLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
